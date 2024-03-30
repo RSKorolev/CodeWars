d9a75c3f03014d3a30f3c15ddef34a9a0163f3c2
Ruslan Korolev <RSKorolev@bk.ru>
2024-03-30 10:29:23 +0700

    added Total amount of points

---
+++ Total amount of points.js
@@ -0,0 +1,18 @@
+function points(games) {
+    let count = 0
+    for (let i = 0; i < games.length; i++) {
+
+        if (games[i][0] > games[i][2]) {
+            count += 3
+        }
+        if (games[i][0] == games[i][2]) {
+            count += 1
+        }
+    }
+    return count
+}
+
+
+
+
+
