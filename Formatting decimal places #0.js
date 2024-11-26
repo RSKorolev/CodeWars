bde8d41526864f22d486eff799ddf73c46c4ad0f
Ruslan Korolev <RSKorolev@bk.ru>
2024-11-26 21:32:18 +0700

    Добавлено преобразование строки в число с помощью "+"

--- Formatting decimal places #0.js
+++ Formatting decimal places #0.js
@@ -1,7 +1,9 @@
 function twoDecimalPlaces(n) {
     // return parseFloat(n.toFixed(2)) // преобразовывает строку в число 
-    return Number(n.toFixed(2))        // изящней чем предыдущий 
+    //return Number(n.toFixed(2))        // изящней чем предыдущий 
+    return +(n.toFixed(2))              // еще одно решение
 
 }
-
-console.log(twoDecimalPlaces(6.7878));
+const n = 6.7878
+console.log(twoDecimalPlaces(n));
+console.log(typeof (+(n.toFixed(2))));  
