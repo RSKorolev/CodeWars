f6863cbf3adb9908f9e523d90bbb506e75ae151e
Ruslan Korolev <RSKorolev@bk.ru>
2024-11-26 23:11:46 +0700

    Switch  через объект

--- Return the day.js
+++ Return the day.js
@@ -10,21 +10,35 @@
 //     else { return "Wrong, please enter a number between 1 and 7" }
 // }
 
+// function whatday(num) {
+
+//     switch (num) {
+//         case 1: return "Sunday"
+//         case 2: return "Monday"
+//         case 3: return "Tuesday"
+//         case 4: return "Wednesday"
+//         case 5: return "Thursday"
+//         case 6: return "Friday"
+//         case 7: return "Saturday"
+//         default: return "Wrong, please enter a number between 1 and 7"
+
+//     }
+// }
+
+// Решение через создание объекта (DeepSeek)
+
 function whatday(num) {
+    const days = {
+        1: "Sunday",
+        2: "Monday",
+        3: "Tuesday",
+        4: "Wednesday",
+        5: "Thursday",
+        6: "Friday",
+        7: "Saturday"
+    };
 
-    switch (num) {
-        case 1: return "Sunday"
-        case 2: return "Monday"
-        case 3: return "Tuesday"
-        case 4: return "Wednesday"
-        case 5: return "Thursday"
-        case 6: return "Friday"
-        case 7: return "Saturday"
-        default: return "Wrong, please enter a number between 1 and 7"
-
-    }
+    return days[num] || "Wrong, please enter a number between 1 and 7";
 }
-
-
 
 console.log(whatday(1));
