5c9ce825bfc8b1c9fce8cdc3647e3d9e8ff1997e
Ruslan Korolev <RSKorolev@bk.ru>
2025-01-18 17:39:55 +0700

    поиск нечетных чисел с помощью Math.floor()

---
+++ Count Odd Numbers below n.js
@@ -0,0 +1,18 @@
+// function oddCount(n) {
+//     let count = 0
+//     for (let i = 1; i < n; i += 2) {
+//         if (i < n) {
+//             count += 1
+//         }
+//     }
+//     return count
+// }
+
+function oddCount(n) {
+    return Math.floor(n / 2)
+}
+
+
+
+
+console.log(oddCount(15));
