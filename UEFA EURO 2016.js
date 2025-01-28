4ab197883ce6c09ba992c589f43ed12aad7fe190
Ruslan Korolev <RSKorolev@bk.ru>
2025-01-28 22:57:32 +0700

    конструкция if...else

---
+++ UEFA EURO 2016.js
@@ -0,0 +1,12 @@
+function uefaEuro2016(teams, scores) {
+    let result;
+    if (scores[0] > scores[1]) {
+        result = `${teams[0]} won!`;
+    } else if (scores[0] < scores[1]) {
+        result = `${teams[1]} won!`;
+    } else {
+        result = "teams played draw.";
+    }
+
+    return `At match ${teams[0]} - ${teams[1]}, ${result}`;
+}
