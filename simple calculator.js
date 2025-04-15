a2d6299e11e149aeea9dd70a9373be00b4fd93c4
RSKorolev <RSKorolev@bk.ru>
2025-04-15 20:23:27 +0700

    конструкция switch

--- simple calculator.js
+++ simple calculator.js
@@ -2,7 +2,7 @@ function calculator(a, b, sign) {
     if (typeof a !== 'number' || typeof b !== 'number') return 'unknown value';
 
     switch (sign) {
-        case '+': return a + b;
+        case '+': return a + b; //  
         case '-': return a - b;
         case '*': return a * b;
         case '/': return a / b;
